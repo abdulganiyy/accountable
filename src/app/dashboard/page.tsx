572952a63e31942ce54320edf3f912f0a5cc8abc
sherif <o.d.alsherif@gmail.com>
2023-11-20 11:28:30 +0100

    feat: dummy video demo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -36,6 +36,13 @@ import { BeatLoader } from "react-spinners";
 import { ToastContainer, toast } from "react-toastify";
 
 const Page = () => {
+    const videoRef = React.useRef(null);
+    const [videoFinished, setVideoFinished] = useState(true);
+
+    const handleVideoEnd = () => {
+      setVideoFinished(true);
+    };
+
 
   const [active, setActive] = useState(1);
   const [callScheduled, setCallScheduled] = useState(false);
@@ -119,180 +126,205 @@ const Page = () => {
               quick steps.
             </p>
           </div>
-          <div className="h-[471px] bg-white border-[1px] border-[#E6E6E6] rounded-2xl mt-8">
-            <div className="h-[68px] flex justify-between items-center px-6 border-b-[1px] border-[#EAEDEF]">
-              <div className="flex gap-x-4 items-center">
-                <span>Get Started</span>
-                <span className="px-2 py-1 bg-[#F2F3F7] text-[#00085A]">
-                  Step {active} out of 4
-                </span>
-              </div>
-              <div className="w-[126px] grid grid-cols-4">
-                {[1, 2, 3, 4].map((item, i) => {
-                  return (
-                    <div
-                      key={i}
-                      className={
-                        "cursor-pointer z-0 relative before:content-[''] before:absolute before:top-[12px] before:z-10 before:translate-x-2/4 before:w-full before:h-[2px] before:bg-[#E6E6E6] before:left-0 last:before:hidden"
-                      }
-                      // onClick={() => setActive(item)}
-                    >
-                      <div
-                        className={twMerge(
-                          "relative z-50 w-[24px] h-[24px] rounded-full bg-[#F2F3F7] text-[#00085A] flex justify-center items-center",
-                          active === item && "bg-[#00085A] text-white",
-                          active > item && "bg-[#00085A] text-white"
-                        )}
+          {videoFinished ? (
+            <>
+              <div className="h-[471px] bg-white border-[1px] border-[#E6E6E6] rounded-2xl mt-8">
+                <div className="h-[68px] flex justify-between items-center px-6 border-b-[1px] border-[#EAEDEF]">
+                  <div className="flex gap-x-4 items-center">
+                    <span>Get Started</span>
+                    <span className="px-2 py-1 bg-[#F2F3F7] text-[#00085A]">
+                      Step {active} out of 4
+                    </span>
+                  </div>
+                  <div className="w-[126px] grid grid-cols-4">
+                    {[1, 2, 3, 4].map((item, i) => {
+                      return (
+                        <div
+                          key={i}
+                          className={
+                            "cursor-pointer z-0 relative before:content-[''] before:absolute before:top-[12px] before:z-10 before:translate-x-2/4 before:w-full before:h-[2px] before:bg-[#E6E6E6] before:left-0 last:before:hidden"
+                          }
+                          // onClick={() => setActive(item)}
+                        >
+                          <div
+                            className={twMerge(
+                              "relative z-50 w-[24px] h-[24px] rounded-full bg-[#F2F3F7] text-[#00085A] flex justify-center items-center",
+                              active === item && "bg-[#00085A] text-white",
+                              active > item && "bg-[#00085A] text-white"
+                            )}
+                          >
+                            {item}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="flex flex-col items-center px-2 pt-12 gap-y-10">
+                  {active === 1 ? (
+                    <Image
+                      alt="logo dashboard"
+                      src="/logomain.svg"
+                      height={72}
+                      width={72}
+                    />
+                  ) : active === 2 ? (
+                    <Image
+                      alt="logo dashboard"
+                      src="/box.svg"
+                      height={72}
+                      width={72}
+                    />
+                  ) : (
+                    <Image
+                      alt="logo dashboard"
+                      src="/vault.svg"
+                      height={72}
+                      width={72}
+                    />
+                  )}
+                  <div className="text-center">
+                    <h4 className="text-[#04050F] font-semibold text-[16px] leading-[26px]">
+                      {active === 1
+                        ? "Get familiar with Accountable"
+                        : active === 2
+                        ? "Meet your Account Manager!"
+                        : active === 3
+                        ? "Link your bank account!"
+                        : "Time to invite your team"}
+                    </h4>
+                    <p className="text-[14px] leading-[22px] max-w-[375px]">
+                      {active === 1
+                        ? ` Let’s get you started with Accountable through a simple
+              walkthrough guide that explains how it works.`
+                        : active === 2
+                        ? `Let's introduce you to your Account Manager. Please schedule a meeting with them as your next step.`
+                        : active === 3
+                        ? `Instantly link your business bank account or upload your bank statement to Accountable to track and monitor your financial data.`
+                        : "Invite your team members to join and manage your accounts together."}
+                    </p>
+                  </div>
+                  <div className="flex gap-x-2">
+                    {active === 1 ? (
+                      <>
+                        <Button
+                          className="w-[184px]"
+                          onClick={() => {
+                            setVideoFinished(!videoFinished);
+                            (videoRef?.current as any)?.play();
+                          }}
+                        >
+                          Start the guide
+                        </Button>
+                        <Button
+                          className="w-[184px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]"
+                          onClick={() => {
+                            setActive(2);
+                          }}
+                        >
+                          Continue
+                        </Button>
+                      </>
+                    ) : active === 2 ? (
+                      <Button
+                        className="w-[300px] px-2"
+                        onClick={() => {
+                          if (!user?.manager) {
+                            toast.error(
+                              "You don't have an account manager yet"
+                            );
+                          } else {
+                            setCallScheduled(true);
+                          }
+                        }}
                       >
-                        {item}
-                      </div>
-                    </div>
-                  );
-                })}
+                        Schedule call with Account Manager
+                      </Button>
+                    ) : active === 3 ? (
+                      <>
+                        <Button
+                          onClick={() => setLinkBanks(true)}
+                          className="w-[220px]"
+                        >
+                          Link your bank account
+                        </Button>
+                        <Button
+                          onClick={() => setUploadStatement(true)}
+                          className="w-[220px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]"
+                        >
+                          Upload a bank statement
+                        </Button>
+                      </>
+                    ) : (
+                      <>
+                        {" "}
+                        <Button
+                          onClick={() => setInvite(true)}
+                          className="w-[188px]"
+                        >
+                          Invite team members
+                        </Button>
+                        <Button className="w-[200px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]">
+                          Skip, I’ll do it later
+                        </Button>
+                      </>
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col items-center px-2 pt-12 gap-y-10">
-              {active === 1 ? (
-                <Image
-                  alt="logo dashboard"
-                  src="/logomain.svg"
-                  height={72}
-                  width={72}
-                />
-              ) : active === 2 ? (
-                <Image
-                  alt="logo dashboard"
-                  src="/box.svg"
-                  height={72}
-                  width={72}
-                />
-              ) : (
-                <Image
-                  alt="logo dashboard"
-                  src="/vault.svg"
-                  height={72}
-                  width={72}
+              {callScheduled && (
+                <CallScheduler
+                  // setActive={setActive}
+                  onClose={() => {
+                    setCallScheduled(false);
+                  }}
+                  successHandler={() => {
+                    setActive(3);
+                  }}
                 />
               )}
-              <div className="text-center">
-                <h4 className="text-[#04050F] font-semibold text-[16px] leading-[26px]">
-                  {active === 1
-                    ? "Get familiar with Accountable"
-                    : active === 2
-                    ? "Meet your Account Manager!"
-                    : active === 3
-                    ? "Link your bank account!"
-                    : "Time to invite your team"}
-                </h4>
-                <p className="text-[14px] leading-[22px] max-w-[375px]">
-                  {active === 1
-                    ? ` Let’s get you started with Accountable through a simple
-              walkthrough guide that explains how it works.`
-                    : active === 2
-                    ? `Let's introduce you to your Account Manager. Please schedule a meeting with them as your next step.`
-                    : active === 3
-                    ? `Instantly link your business bank account or upload your bank statement to Accountable to track and monitor your financial data.`
-                    : "Invite your team members to join and manage your accounts together."}
-                </p>
-              </div>
-              <div className="flex gap-x-2">
-                {active === 1 ? (
-                  <>
-                    <Button className="w-[184px]" onClick={() => {}}>
-                      Start the guide
-                    </Button>
-                    <Button
-                      className="w-[184px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]"
-                      onClick={() => {
-                        setActive(2);
-                      }}
-                    >
-                      Continue
-                    </Button>
-                  </>
-                ) : active === 2 ? (
-                  <Button
-                    className="w-[300px] px-2"
-                    onClick={() => {
-                      if (!user?.manager) {
-                        toast.error("You dont have an account manager yet");
-                      } else {
-                        setCallScheduled(true);
-                      }
-                    }}
-                  >
-                    Schedule call with Account Manager
-                  </Button>
-                ) : active === 3 ? (
-                  <>
-                    <Button
-                      onClick={() => setLinkBanks(true)}
-                      className="w-[220px]"
-                    >
-                      Link your bank account
-                    </Button>
-                    <Button
-                      onClick={() => setUploadStatement(true)}
-                      className="w-[220px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]"
-                    >
-                      Upload a bank statement
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    {" "}
-                    <Button
-                      onClick={() => setInvite(true)}
-                      className="w-[188px]"
-                    >
-                      Invite team members
-                    </Button>
-                    <Button className="w-[200px] bg-white text-[#071A7E] border-[2px] border-[#EAEDEF]">
-                      Skip, I’ll do it later
-                    </Button>
-                  </>
-                )}
-              </div>
+              {uploadStatement && (
+                <UploadBankStatement
+                  onClose={() => {
+                    setUploadStatement(false);
+                  }}
+                  successHandler={() => {
+                    // setAccountLinked(true);
+                  }}
+                />
+              )}
+              {linkBanks && (
+                <LinkBanks
+                  onClose={() => {
+                    setLinkBanks(false);
+                  }}
+                  successHandler={() => {
+                    setAccountLinked(true);
+                    result.refetch();
+                  }}
+                />
+              )}
+              {invite && (
+                <InviteTeamMembers
+                  onClose={() => {
+                    setInvite(false);
+                  }}
+                />
+              )}
+            </>
+          ) : (
+            <div className="xl:h-[471px] mt-8 overflow-hidden border-[1px] border-[#E6E6E6] rounded-2xl">
+              <video
+                className="w-full h-full "
+                ref={videoRef}
+                autoPlay
+                src="./video.mp4"
+                muted
+                controls
+                onEnded={handleVideoEnd}
+                poster="./images/image 18.png"
+              ></video>
             </div>
-          </div>
-          {callScheduled && (
-            <CallScheduler
-              // setActive={setActive}
-              onClose={() => {
-                setCallScheduled(false);
-              }}
-              successHandler={() => {
-                setActive(3);
-              }}
-            />
-          )}
-          {uploadStatement && (
-            <UploadBankStatement
-              onClose={() => {
-                setUploadStatement(false);
-              }}
-              successHandler={() => {
-                // setAccountLinked(true);
-              }}
-            />
-          )}
-          {linkBanks && (
-            <LinkBanks
-              onClose={() => {
-                setLinkBanks(false);
-              }}
-              successHandler={() => {
-                setAccountLinked(true);
-                result.refetch();
-              }}
-            />
-          )}
-          {invite && (
-            <InviteTeamMembers
-              onClose={() => {
-                setInvite(false);
-              }}
-            />
           )}
         </div>
       ) : (
